9a56388363fb712d1ba4e33c904944fa0d3f9d9a
Dipesh Acharya <xtranophilist@gmail.com>
2018-04-09 14:21:11 +0545

    Create unit tests for hamburger toggle with jest

--- test/hamburger.test.js
+++ test/hamburger.test.js
@@ -4,9 +4,25 @@ import Header from '../components/Header.vue'
 describe('Hamburger Test', () => {
 
   const wrapper = mount(Header);
+  const clickable = wrapper.find('.navbar-burger span:first-child');
 
-  it('renders the correct markup', () => {
-    expect('abc').toContain('d')
-  })
-  
+  it('Header is a component', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  });
+
+  it('Hamburger is hidden', () => {
+    expect(wrapper.vm.showNav).toBe(false);
+  });
+
+  it('Clicking Hamburger shows it', () => {
+    expect(wrapper.vm.showNav).toBe(false);
+    clickable.trigger('click');
+    expect(wrapper.vm.showNav).toBe(true);
+  });
+
+  it('Clicking Hamburger again shows it', () => {
+    clickable.trigger('click');
+    expect(wrapper.vm.showNav).toBe(false);
+  });
+
 });
