2e6406a349991aeeca77f09b11f5de0e32536b39
Dipesh Acharya <xtranophilist@gmail.com>
2018-04-16 18:48:55 +0545

    Create empty institute page

---
+++ pages/_slug/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Institute Page
+    </div>
+</template>
+
+<script>
+  export default {
+    validate({params}) {
+      // Must be a number
+      return /^[A-Za-z0-9]+(?:-[A-Za-z0-9]+)*$/.test(params.id)
+    }
+  }
+</script>
