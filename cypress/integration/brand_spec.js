c7a7ac1488eef3be2cb65c2c17bd895af60e2219
Dipesh Acharya <xtranophilist@gmail.com>
2018-04-24 20:01:46 +0545

    Convert branding test to es6

--- cypress/integration/brand_spec.js
+++ cypress/integration/brand_spec.js
@@ -1,15 +1,16 @@
 const base_url = 'http://127.0.0.1:3000';
 
-describe('Branding', function () {
+describe('Branding', () => {
   context('Homepage', function () {
 
-    beforeEach(function () {
+    beforeEach(() => {
       cy.visit(base_url);
     });
 
-    it('.should() - assert that <title> is correct', function () {
-      cy.title().toUpperCase().should('include', 'edusanjal')
+    it('Has correct title', () => {
+      cy.title().should('include', 'edusanjal')
     });
+
   });
 
 });
