601d339231451002474813c6ae11956a0b3284ee
Dipesh Acharya <xtranophilist@gmail.com>
2018-05-29 16:58:08 +0545

    Improve style for listing page

--- pages/colleges.vue
+++ pages/colleges.vue
@@ -1,9 +1,33 @@
 <template>
-    <div>
+    <div class="bg-grey p1">
         <ul v-for="obj in objs" :key="obj.slug">
-            <div class="card-content info">
-                <img :src="obj.logo_set.small" :alt="obj.name" :title="obj.name"/>
-                <nuxt-link :to="{name: 'slug', params: {slug: obj.slug}}">{{obj.name}}</nuxt-link>
+            <div class="card mt1 mh1">
+                <div class="card-content info list-item">
+                    <nuxt-link :to="{name: 'slug', params: {slug: obj.slug}}" class="list-image">
+                        <img :src="obj.logo_set.small" :alt="obj.name" :title="obj.name"/>
+                    </nuxt-link>
+                    <div class="list-detail">
+                        <div>
+                            <h3>
+                                <nuxt-link :to="{name: 'slug', params: {slug: obj.slug}}">{{obj.name}}</nuxt-link>
+                            </h3>
+                            <div v-if="obj.address">
+                                <FA i="location-arrow"/>
+                                {{obj.address}}
+                            </div>
+                            <div v-if="obj.type">
+                                <FA i="building"/>
+                                {{obj.type}}
+                            </div>
+                            <div class="boards" v-if="obj.boards && obj.boards.length">
+                                <FA i="university"/>
+                                <div class="csv">
+                                    <div v-for="board in obj.boards" :key="board.slug">{{board.name}}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </ul>
--- plugins/fontAwesome.js
+++ plugins/fontAwesome.js
@@ -7,5 +7,6 @@ import AngleLeft from '@fortawesome/fontawesome-free-solid/faAngleLeft'
 import AngleRight from '@fortawesome/fontawesome-free-solid/faAngleRight'
 import At from '@fortawesome/fontawesome-free-solid/faAt'
 import Globe from '@fortawesome/fontawesome-free-solid/faGlobe'
+import University from '@fortawesome/fontawesome-free-solid/faUniversity'
 
-fontawesome.library.add(LocationArrow, Building, Phone, AngleLeft, AngleRight, At, Globe);
+fontawesome.library.add(LocationArrow, Building, Phone, AngleLeft, AngleRight, At, Globe, University);
